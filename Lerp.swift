d2721662eed5bdc6e535362d9bd89748337fbdd5
Daniel Clelland <daniel.clelland@gmail.com>
2015-12-06 15:48:19 +1300

    Fixed docs

--- Lerp.swift
+++ Lerp.swift
@@ -24,6 +24,16 @@ func lerp(weighting: Double, min: Double, max: Double) -> Double {
     return min + (weighting * (max - min))
 }
 
+/**
+Calculates the linear interpolation between a minimum and a maximum value. Identical to `lerp()`.
+
+- parameter weighting: The input weighting. Typically in the range `0...1` but other values are supported.
+- parameter min: The minimum value.
+- parameter max: The maximum value.
+
+- returns: The weighted average. Typically in the range `min...max` but other values are supported.
+*/
+
 func lerpf(weighting: Float, min: Float, max: Float) -> Float {
     return min + (weighting * (max - min))
 }
@@ -43,6 +53,16 @@ Calculates the inverse linear interpolation between a minimum and a maximum valu
 func ilerp(value: Double, min: Double, max: Double) -> Double {
     return (value - min) / (max - min)
 }
+
+/**
+Calculates the inverse linear interpolation between a minimum and a maximum value. Identical to `ilerp()`.
+
+- parameter value: The input value. Typically in the range `min...max` but other values are supported.
+- parameter min: The minimum value.
+- parameter max: The maximum value.
+
+- returns: The weighting of *value* between *min* and *max*. Typically in the range `0...1` but other values are supported.
+*/
 
 func ilerpf(value: Float, min: Float, max: Float) -> Float {
     return (value - min) / (max - min)
